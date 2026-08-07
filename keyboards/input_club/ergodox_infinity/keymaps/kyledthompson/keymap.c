40cb21ca2122d442366743fc547c25e4b9975652
KyleDThompson <95723443+KyleDThompson@users.noreply.github.com>
2026-08-07 14:46:11 -0600

    Implement bongo cat animation in keymap
    Added bongo cat animation to the display based on typing speed.

--- keyboards/input_club/ergodox_infinity/keymaps/kyledthompson/keymap.c
+++ keyboards/input_club/ergodox_infinity/keymaps/kyledthompson/keymap.c
@@ -4,6 +4,9 @@
 
 char wpm_str[10];
 
+static uint32_t cat_timer = 0;
+static bool cat_frame = false;
+
 enum custom_layers {
     BASE,   // default layer
     NUMBER,
@@ -270,6 +273,60 @@ void matrix_scan_user(void) {
 
 //Test of different visualizer code
 #ifdef ST7565_ENABLE
+void update_cat_animation(void) {
+    uint8_t wpm = get_current_wpm();
+
+    // Animate only when typing
+    if (wpm > 20) {
+
+        // Faster animation at higher WPM
+        uint16_t speed = 250;
+
+        if (wpm > 40) speed = 150;
+        if (wpm > 80) speed = 75;
+
+        if (timer_elapsed32(cat_timer) > speed) {
+            cat_frame = !cat_frame;
+            cat_timer = timer_read32();
+        }
+    } else {
+        cat_frame = false;
+    }
+}
+
+void render_bongo_cat(void) {
+
+    update_cat_animation();
+
+    // Idle cat
+    if (get_current_wpm() <= 20) {
+
+        st7565_write_ln_P(PSTR("/\\_/\\ "), false);
+        st7565_write_ln_P(PSTR("( o.o )"), false);
+        st7565_write_ln_P(PSTR(" > ^ < "), false);
+        st7565_write_ln_P(PSTR("/^\\ /^\\ "), false);
+
+        return;
+    }
+
+    // Animated paws
+    if (cat_frame) {
+
+        st7565_write_ln_P(PSTR("/\\_/\\ "), false);
+        st7565_write_ln_P(PSTR("( ^.^ )"), false);
+        st7565_write_ln_P(PSTR(" > ^ < "), false);
+        st7565_write_ln_P(PSTR("/^\\ ,., "), false);
+
+    } else {
+
+        st7565_write_ln_P(PSTR("/\\_/\\ "), false);
+        st7565_write_ln_P(PSTR("( ^.^ )"), false);
+        st7565_write_ln_P(PSTR(" > ^ < "), false);
+        st7565_write_ln_P(PSTR(",., /^\\ "), false);
+    }
+}
+`
+
 void st7565_task_user(void) {
 
     if (is_keyboard_master()) {
@@ -308,10 +365,7 @@ void st7565_task_user(void) {
         }
     } else {
         // Draw logo (Right side)
-        static const char qmk_logo[] = {
-            0x80, 0x81, 0x82, 0x83, 0x84, 0x85, 0x86, 0x87, 0x88, 0x89, 0x8A, 0x8B, 0x8C, 0x8D, 0x8E, 0x8F, 0x90, 0x91, 0x92, 0x93, 0x94,
-            0xA0, 0xA1, 0xA2, 0xA3, 0xA4, 0xA5, 0xA6, 0xA7, 0xA8, 0xA9, 0xAA, 0xAB, 0xAC, 0xAD, 0xAE, 0xAF, 0xB0, 0xB1, 0xB2, 0xB3, 0xB4,
-            0xC0, 0xC1, 0xC2, 0xC3, 0xC4, 0xC5, 0xC6, 0xC7, 0xC8, 0xC9, 0xCA, 0xCB, 0xCC, 0xCD, 0xCE, 0xCF, 0xD0, 0xD1, 0xD2, 0xD3, 0xD4, 0x00
+        render_bongo_cat();
         };
 
         st7565_write(qmk_logo, false);
